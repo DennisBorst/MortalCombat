8f80a633a67bb59b39aacfe622b71c3b2196d7d0
Coda <Errorcodebin_2@hotmail.com>
2021-08-30 22:18:38 +0200

    Update Character Skin Behavior

--- MortalCombat/Assets/CharacterSkinSelection.cs
+++ MortalCombat/Assets/CharacterSkinSelection.cs
@@ -6,7 +6,7 @@ namespace MortalCombat
 {
     public class CharacterSkinSelection : MonoBehaviour
     {
-        //[SerializeField] private GameObject currentSkin = null;
+        [SerializeField] private GameObject currentSkin = null;
         [SerializeField] private GameObject character = null;
         [SerializeField] private CharacterID characterId = null;
 
@@ -17,9 +17,13 @@ namespace MortalCombat
             
             if (characterId)
             {
+                if (currentSkin)
+                    Object.Destroy(currentSkin);
+
                 GameObject skin = PlayerConfiguration.Instance.GetPlayerSkin(characterId.m_PlayerID);
                 var obj = Instantiate(skin, character.transform, false);
                 obj.name = "CharacterObject";
+                currentSkin = obj;
 
                 character.SetActive(true);
             }
