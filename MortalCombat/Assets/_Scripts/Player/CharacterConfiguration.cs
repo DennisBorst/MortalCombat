52d7257c9db3bd6f07492c095affeff90ca6b9d0
Coda <Errorcodebin_2@hotmail.com>
2022-02-13 02:46:47 +0100

    Character configuration is now done per scriptable object

---
+++ MortalCombat/Assets/_Scripts/Player/CharacterConfiguration.cs
@@ -0,0 +1,22 @@
+using ToolBox;
+using UnityEngine;
+
+#pragma warning disable IDE0044 // Add readonly modifier
+namespace MortalCombat
+{
+    public class CharacterConfiguration : ScriptableObject, IIdentifiable<Character>
+    {
+        [SerializeField] private Character _Character = Character.Blue;
+        [SerializeField] private GameObject _Skin = null;
+        [SerializeField] private Sprite _PlayerIconSprite = null;
+        [SerializeField] private Sprite _CharacterSelectSprite = null;
+
+        public Character Character => _Character;
+        public GameObject Skin => _Skin;
+        public Sprite CharacterSelectSprite => _CharacterSelectSprite;
+        public Sprite PlayerIconSprite => _PlayerIconSprite;
+
+        Character IIdentifiable<Character>.Id => Character;
+    }
+}
+#pragma warning restore IDE0044 // Add readonly modifier
