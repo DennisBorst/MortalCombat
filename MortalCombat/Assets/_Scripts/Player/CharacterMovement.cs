9bfc99f8247ebf20bdeb602b5bc1af0671eac67a
DennisBorst <dennis.borst@hotmail.nl>
2022-02-19 18:06:29 +0100

    character stats update

--- MortalCombat/Assets/_Scripts/Player/CharacterMovement.cs
+++ MortalCombat/Assets/_Scripts/Player/CharacterMovement.cs
@@ -15,6 +15,7 @@ namespace MortalCombat
         [SerializeField] private float m_JumpForce = 15f;                          // Amount of force added when the player jumps.
         [SerializeField] private float m_DoubleJumpForce = 10f;                          // Amount of force added when the player jumps.
         [Range(0, 1), SerializeField] private float m_CrouchSpeed = .36f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
+        [SerializeField] private float movementSpeed = 7.5f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
         [Range(0, .3f), SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
         [SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
         [SerializeField] private float m_ShootCooldown;
@@ -209,7 +210,7 @@ namespace MortalCombat
             if (m_Grounded || m_AirControl)
             {
                 // Move the character by finding the target velocity
-                Vector3 targetVelocity = new Vector2(move * 10f, m_rb.velocity.y);
+                Vector3 targetVelocity = new Vector2(move * movementSpeed, m_rb.velocity.y);
                 // And then smoothing it out and applying it to the character
                 m_rb.velocity = Vector3.SmoothDamp(m_rb.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
 
